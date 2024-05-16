c09422acc679ce10dd810ba368158b135d9c3d1d
carl.hummerhielm <carl.hummerhielm@elev.ga.ntig.se>
2024-05-16 16:45:13 +0200

    Har putsat till koden lite och lagt till kommentarer

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,44 @@ import java.net.MulticastSocket;
 
 public class Main {
     public static void main(String[] args) {
+
         int portnumber = 8880;
-        if (args.length >= 1){
+        if (args.length >= 1) {
             portnumber = Integer.parseInt(args[0]);
         }
-        try{
+
+        try {
             MulticastSocket chatMulticastSocket = new MulticastSocket(portnumber);
-
             InetAddress group = InetAddress.getByName("224.4.5.6");
-
             chatMulticastSocket.joinGroup(group);
-            //Skickar
-            String msg = "";
+
+            String msg;
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Skriv ett enkelt matte tal(+, -, * eller /): ");
+            System.out.println("Skriv ett enkelt matte tal(+, -, * eller /)");
+            System.out.print(": ");
             msg = br.readLine();
-            while(true) {
+
+            while (true) {
+                //Skickar datan
                 DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
                 chatMulticastSocket.send(data);
+
                 //Tar emot datan
                 byte buf[] = new byte[1024];
                 DatagramPacket resultatData = new DatagramPacket(buf, buf.length);
                 chatMulticastSocket.receive(resultatData);
                 String resultat = new String(resultatData.getData(), 0, resultatData.getLength());
+
+                //Skippar ifall svaret är samma sak som det som skickades till servern
                 if (resultat.equals(msg)) {
                     continue;
                 }
-
-                // Annars skriv ut resultatet
+                //Annars skrivs resultatet ut
                 System.out.println(resultat);
                 break;
             }
-        }catch (IOException ie){
+
+        } catch (IOException ie) {
             ie.printStackTrace();
         }
     }
